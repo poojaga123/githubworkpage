4f7deb085b3a8db7d823ebc82eaa9e059067d63b
poojaga123 <91052443+poojaga123@users.noreply.github.com>
2021-09-21 19:06:48 +1100

    Create program2.java
    convert minutes unto days and year format

---
+++ program2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class program2
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int m;
+    m=sc.nextInt();
+    int years,days;
+    int cn=60*24*365;
+    year=a/cn;
+    System.out.println(year+"year");
+    a=a-(year*cn);
+    int cm=60*24;
+    days=a/cm;
+    System.out.println(days"+days");
+  }
+}
+    
